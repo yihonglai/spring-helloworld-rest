1e7bfe00bf5b8d5aad9828ae5b5611c86d924ba3
yihonglai <yihong.lai@gmail.com>
2019-10-28 23:28:18 +0800

    Initial code

---
+++ src/main/java/com/yihong/helloworldrest/HelloworldrestApplication.java
@@ -0,0 +1,13 @@
+package com.yihong.helloworldrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldrestApplication.class, args);
+    }
+
+}
